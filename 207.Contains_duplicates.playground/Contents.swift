2eb7304213e4f450b4bbcbc8001e031bc5b5f0fc
Igor Polousov <igorpolousov@gmail.com>
2025-01-21 14:29:14 +0300

    207.Contains_Duplicates

---
+++ 207.Contains_duplicates.playground/Contents.swift
@@ -0,0 +1,53 @@
+import UIKit
+
+
+/*
+
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+
+Output: true
+
+Explanation:
+
+The element 1 occurs at the indices 0 and 3.
+
+ 
+Example 2:
+
+Input: nums = [1,2,3,4]
+
+Output: false
+
+Explanation:
+
+All elements are distinct.
+
+ 
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+
+Output: true
+
+*/
+
+ let nums = [1,1,1,3,3,4,3,2,4,2]
+func containsDuplicates(_ nums: [Int]) -> Bool {
+    var set = Set<Int>()
+    for num in nums {
+        if set.contains(num) {
+            return true
+        } else {
+            set.insert(num)
+        }
+    }
+    return false
+}
+
+containsDuplicates(nums)
