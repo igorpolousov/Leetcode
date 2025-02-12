e1a43aa03edb64e6ab61676b700bdf2376d69dd9
Igor Polousov <igorpolousov@gmail.com>
2025-02-12 14:36:19 +0300

    232.Implement queue using stack

---
+++ 232.Implement_Queue_using_stack.playground/Contents.swift
@@ -0,0 +1,67 @@
+import UIKit
+
+/*
+ Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
+
+ Implement the MyQueue class:
+
+ void push(int x) Pushes element x to the back of the queue.
+ int pop() Removes the element from the front of the queue and returns it.
+ int peek() Returns the element at the front of the queue.
+ boolean empty() Returns true if the queue is empty, false otherwise.
+ Notes:
+
+ You must use only standard operations of a stack, which means only push to top, peek/pop from top, size, and is empty operations are valid.
+ Depending on your language, the stack may not be supported natively. You may simulate a stack using a list or deque (double-ended queue) as long as you use only a stack's standard operations.
+ 
+ 
+ Example:
+ 
+ Input
+ ["MyQueue", "push", "push", "peek", "pop", "empty"]
+ [[], [1], [2], [], [], []]
+ Output
+ [null, null, null, 1, 1, false]
+
+ Explanation
+ MyQueue myQueue = new MyQueue();
+ myQueue.push(1); // queue is: [1]
+ myQueue.push(2); // queue is: [1, 2] (leftmost is front of the queue)
+ myQueue.peek(); // return 1
+ myQueue.pop(); // return 1, queue is [2]
+ myQueue.empty(); // return false
+ 
+ */
+
+
+class MyQueue {
+    
+    var myQueue: [Int]
+    
+    init(myQueue: [Int] = []) {
+        self.myQueue = myQueue
+    }
+    
+    func push(_ x: Int) {
+        myQueue.append(x)
+    }
+    
+    func pop() -> Int {
+        let ans = myQueue.first
+        myQueue.remove(at: 0)
+        return ans ?? 0
+    }
+    
+    func peek() -> Int {
+        return myQueue.first ?? 0
+    }
+    
+    func empty() -> Bool {
+        myQueue.isEmpty
+    }
+}
+
+let q1 = MyQueue()
+q1.push(20)
+q1.push(21)
+print(q1.myQueue)
